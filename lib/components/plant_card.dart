3021ac941ff4296d226a95e4112f83d433b9c432
githubjerin <100055207+githubjerin@users.noreply.github.com>
2024-05-18 19:16:37 +0530

    componentize

---
+++ lib/components/plant_card.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/cupertino.dart';
+
+import '../constants.dart';
+import 'curve.dart';
+
+class PlantCard extends StatelessWidget {
+  const PlantCard({
+    required this.plantType,
+    required this.plantName,
+    required this.plantPrice,
+    required this.image,
+    required this.onTap,
+    super.key,
+  });
+
+  final String plantType;
+  final String plantName;
+  final double plantPrice;
+  final Image image;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Container(
+            height: 220.0,
+            width: 185.0,
+            decoration: BoxDecoration(
+              color: kGinColor,
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: CustomPaint(
+              painter: CurvePainter(),
+            ),
+          ),
+          Positioned(
+            // height: 240.0,
+            // width: 124.0,
+            left: 8.0,
+            bottom: 70.0,
+            child: Container(
+              constraints:
+              const BoxConstraints(maxWidth: 124.0, maxHeight: 240.0),
+              child: Hero(tag: plantName, child: image),
+            ),
+          ),
+          Positioned(
+            bottom: 0.0,
+            left: 0.0,
+            child: Container(
+              width: 185,
+              height: 60.0,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+              child: Padding(
+                padding:
+                const EdgeInsets.symmetric(vertical: 10.0, horizontal: 8.0),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Flexible(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            plantType,
+                            style: TextStyle(
+                              color: kDarkGreenColor,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          const SizedBox(height: 2.0),
+                          Expanded(
+                            child: Text(
+                              plantName,
+                              overflow: TextOverflow.fade,
+                              maxLines: 1,
+                              softWrap: false,
+                              style: TextStyle(
+                                color: kDarkGreenColor,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w600,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 6.0,
+                        horizontal: 10.0,
+                      ),
+                      decoration: BoxDecoration(
+                        color: kFoamColor,
+                        borderRadius: BorderRadius.circular(20.0),
+                      ),
+                      constraints: const BoxConstraints(maxWidth: 90.0),
+                      child: Text(
+                        '₹${plantPrice}0',
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        softWrap: false,
+                        style: TextStyle(
+                          color: kDarkGreenColor,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 12.8,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/components/recently_viewed_card.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/cupertino.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../constants.dart';
+
+class RecentlyViewedCard extends StatelessWidget {
+  const RecentlyViewedCard({
+    required this.plantName,
+    required this.plantInfo,
+    required this.image,
+    super.key,
+  });
+
+  final String plantName;
+  final String plantInfo;
+  final ImageProvider<Object> image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Container(
+          width: 60.0,
+          height: 60.0,
+          alignment: Alignment.center,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: image,
+            ),
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+        ),
+        const SizedBox(width: 24.0),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              plantName,
+              style: GoogleFonts.poppins(
+                color: kDarkGreenColor,
+                fontSize: 16.0,
+                fontWeight: FontWeight.w500,
+              ),
+            ),
+            Text(
+              plantInfo,
+              style: GoogleFonts.poppins(
+                color: kGreyColor,
+              ),
+            )
+          ],
+        ),
+      ],
+    );
+  }
+}
